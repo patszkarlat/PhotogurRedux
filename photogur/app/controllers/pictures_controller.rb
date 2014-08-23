087472f263904db31b950809602e2d1b93c6df72
patszkarlat <punkgeek@mac.com>
2014-08-23 17:50:27 -0400

    Making each pic show up at it's own URL using static routes

--- photogur/app/controllers/pictures_controller.rb
+++ photogur/app/controllers/pictures_controller.rb
@@ -1,21 +1,41 @@
 class PicturesController < ApplicationController
 	def index
-		@pictures = [
-			{
+
+		def picture0
+		
+		@picture = {
+			
 				:title => "The old church on the coast of White sea",
 				:artist => "Serghey Ershov",
 				:url => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
-			},
-			{
+
+			}
+			
+		end
+
+		def picture1
+
+			@picture = {
+
 				:title => "Sea Power",
 				:artist => "Stephen Scullion",
 				:url => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
-				},
-				{
+				
+				}
+
+		end
+
+		def picture2
+
+			@picture  = {
+
 					:title => "Into the Poppies",
 					:artist => "John Wilhelm",
 					:url => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
+				
 				}
-		]
+
+			end
+		
 	end
 end
