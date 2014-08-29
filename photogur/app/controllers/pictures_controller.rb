ba82fa0c695f6b6b39198718dd7986f64fbe37a9
patszkarlat <punkgeek@mac.com>
2014-08-29 14:25:07 -0400

    Moving @pictures in pics_controller.rb into database

--- photogur/app/controllers/pictures_controller.rb
+++ photogur/app/controllers/pictures_controller.rb
@@ -1,70 +1,20 @@
 class PicturesController < ApplicationController
 	
 	def index
+		@pictures = Picture.all
+	end
+
+	def show
+		@picture = Picture.find(params[:id])
+	end
+
+
+	def new
+	end
+
+	def create
+			render :text => "Saving a picture. URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
+	end
 
 	
-		@pictures = [
-			{
-
-				:title => "The old church on the coast of White sea",
-				:artist => "Serghey Ershov",
-				:url => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
-
-			},
-			{
-
-
-				:title => "Sea Power",
-				:artist => "Stephen Scullion",
-				:url => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
-				
-				},
-				{
-
-
-					:title => "Into the Poppies",
-					:artist => "John Wilhelm",
-					:url => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
-				
-				}
-			]
-	end
-		
-
-		def show
-			@pictures = [
-				{
-
-
-				:title => "The old church on the coast of White sea",
-				:artist => "Serghey Ershov",
-				:url => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
-
-				},
-				{
-
-
-				:title => "Sea Power",
-				:artist => "Stephen Scullion",
-				:url => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
-
-				},
-				{
-
-				:title => "Into the Poppies",
-				:artist => "John Wilhelm",
-				:url => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
-
-				}
-			]
-			@picture = @pictures[params[:id].to_i]
-		
-		end
-
-		def new
-		end
-
-		def create
-			render :text => "Saving a picture. URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
-		end
 	end
